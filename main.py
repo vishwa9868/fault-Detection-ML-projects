8934b22a8e879e87c7cdb0727d8a53f157c1102b
vishwa9868 <vishwanathpatil023@gmail.cpm>
2023-06-04 08:40:16 +0530

    updated

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys,os
 from sensor.entity.config_entity import DataIngestionConfig
 
 try:
-     logging.info("Starting the test_logger_and_exception"):
+     logging.info("Starting the test_logger_and_exception")
      result = 3/0
      print(result)
 except Exception as e:
@@ -16,5 +16,13 @@ except Exception as e:
 if __name__=="__main__":
      try:
           test_logger_and_exception()
+     except Exception as e:
+          print(e)
+
+if __name__=="__main__":
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config)
+          print(data_ingestion_config.to_dict())
      except Exception as e:
           print(e)
--- sensor/entity/config_entity.py
+++ sensor/entity/config_entity.py
@@ -3,6 +3,14 @@ from sensor.exception import SensorException
 from sensor.logger import logging
 from datetime import datetime
 
+if __name__ == "__main__":
+    try:
+        training_pipeline_config = config_entity.TrainingPipelineConfig()
+        data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+        print(data_ingestion_config.to_dict())
+    except Exception as e:
+        print(e)
+
 
 
 FILE_NAME = "sensor.csv"
