6af69f22cfe6a430fabf1facc841ec44ea96807c
vishwa9868 <vishwanathpatil023@gmail.cpm>
2023-06-04 14:25:11 +0530

    updated

--- main.py
+++ main.py
@@ -27,5 +27,7 @@ if __name__=="__main__":
           training_pipeline_config = config_entity.TrainingPipelineConfig()
           data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+          print(data_ingestion.initiate_data_ingestion())
      except Exception as e:
           print(e)
